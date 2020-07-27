4bd1e596fbc4692a5c06aa280ebd3378366f7e12
Anthony <ruffina@evms.edu>
2020-07-27 14:42:06 -0400

    reverted url

--- vr-photos.js
+++ vr-photos.js
@@ -3,7 +3,7 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
   // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
-    image: '//www.evms.edu/media/training/student-parking-garage.jpg',
+    image: 'https://www.evms.edu/media/training/student-parking-garage.jpg',
     is_stereo: true
   });
 }
